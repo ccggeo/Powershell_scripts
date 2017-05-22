2d58add4605b050fece151df580285c8a4615e79
Connor George <cgeorge@blacksun.com>
2017-05-22 14:38:54 +0100

    Smb1 check added

---
+++ smb1enabled.ps1
@@ -0,0 +1,16 @@
+﻿#Grabs workstations dependant on naming scheme 
+$name = Get-ADComputer -filter * | ? { $_.Name -like "*name*" }  
+
+$name.name 
+
+
+$name | % {
+
+invoke-command -ComputerName $_.name -ScriptBlock{
+
+$smb1enabled = (Get-WindowsOptionalFeature -online | Where-Object {$_.FeatureName -eq "smb1protocol"}).state
+ 
+$smb1enabled
+}
+
+}
