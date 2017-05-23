0ba4c613ebe200a4a3568bf05b21dab7834f7ce1
ccggeo <georgeconnorc@gmail.com>
2017-05-23 09:50:33 +0100

    Checks what source a host is using for NTP

---
+++ time_source_.ps1
@@ -0,0 +1,23 @@
+﻿$computers= Get-ADComputer-SearchBase "searchbase"-Filter {enabled -eq $true} 
+ 
+$output= @()
+$computers| % {
+    
+    $line= '' | Select DNSHostname,Timesource
+    $line.DNSHostname = $_.DNSHostname
+    Write-Host -ForegroundColorYellow "Testing $($_.DNSHostname)"
+    if (Test-WSMan$_.DNSHostname -ErrorAction SilentlyContinue )
+    {
+        Write-Host -ForegroundColorGreen "Connected to $($_.DNSHostname)"
+        $line.Timesource = Invoke-Command-ComputerName $_.DNSHostname -ScriptBlock { w32tm/query /source}
+    }
+        else {
+            Write-Host -ForegroundColorRed "Cannot connect to $($_.DNSHostname)"
+            $line.Timesource = "Error"
+        }
+        $line
+        $output +=$line
+}
+ 
+$output| Out-GridView
+ 
