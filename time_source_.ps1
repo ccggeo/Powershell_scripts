bce4124968dd1a72b64fe7314dff8bea5444f965
ccggeo <georgeconnorc@gmail.com>
2017-05-23 09:55:02 +0100

    Improved by added ifelse to check if service is running and if not, turn it on. this is after ping and time source checks.

--- time_source_.ps1
+++ time_source_.ps1
@@ -1,23 +1,46 @@
-﻿$computers= Get-ADComputer-SearchBase "searchbase"-Filter {enabled -eq $true} 
+﻿$computers= Get-ADComputer -SearchBase "searchbase"-Filter {enabled -eq $true} 
  
 $output= @()
 $computers| % {
     
     $line= '' | Select DNSHostname,Timesource
     $line.DNSHostname = $_.DNSHostname
-    Write-Host -ForegroundColorYellow "Testing $($_.DNSHostname)"
-    if (Test-WSMan$_.DNSHostname -ErrorAction SilentlyContinue )
+   # Start-Transcript
+    Write-Host -ForegroundColor Yellow "Testing $($_.DNSHostname)"
+    if (Test-WSMan $_.DNSHostname -ErrorAction SilentlyContinue )
+
     {
-        Write-Host -ForegroundColorGreen "Connected to $($_.DNSHostname)"
-        $line.Timesource = Invoke-Command-ComputerName $_.DNSHostname -ScriptBlock { w32tm/query /source}
+        Write-Host -ForegroundColor Green "Connected to $($_.DNSHostname)"
+       
+        $line.Timesource = Invoke-Command -ComputerName $_.DNSHostname -ScriptBlock { w32tm /query /source}
+      
+      
+      ### check if service is running
+        $time_service = Invoke-Command -ComputerName $_ -ScriptBlock { get-service -Name w32time}
+         if ( $time_service.status -eq "running") 
+            {write-host -foregroundcolor green "----------Service is running----------"}
+            
+         else {write-host -ForegroundColor red "Time service is not running attempting to start service"
+               Invoke-Command -ComputerName $_.DNSHostname -ScriptBlock { 
+               start-service -Name w32time
+               $running? =  get-service -Name w32time
+               write-host -foregroundcolor green "Time server status:" $running?.status
+             }
+             }
+
+
     }
+
         else {
-            Write-Host -ForegroundColorRed "Cannot connect to $($_.DNSHostname)"
+            Write-Host -ForegroundColor Red "Cannot connect to $($_.DNSHostname)"
             $line.Timesource = "Error"
+
         }
         $line
         $output +=$line
 }
  
-$output| Out-GridView
+
  
+$timestamp = $(get-date -Format ddMMyyyy)
+$output | export-csv "c:\temp\Time_server_exports\$timestamp.csv"
