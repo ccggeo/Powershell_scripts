371e42bda45fea438136a15dd293b95b5033bad0
ccggeo <georgeconnorc@gmail.com>
2017-06-05 17:27:49 +0100

    Script to check last reboot time of pc. Can run on schedule

---
+++ last_rebootime.ps1
@@ -0,0 +1,24 @@
+﻿$machines=@()
+
+$name = Get-ADComputer -filter * | ? { $_.Name -like "**"  } 
+
+
+$machines
+
+$output =@()
+
+foreach ($comp in $machines){
+    
+    $line = '' | select comp, lastboot 
+
+    $state = invoke-command -computername $comp -scriptblock {Get-WmiObject win32_operatingsystem | select csname, @{LABEL=’LastBootUpTime’;EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}
+                                                             }
+        
+        $line.comp = $comp
+        $line.lastboot = $state.lastbootuptime
+         
+        $output += $line
+                            }
+
+$output | Out-GridView 
+
