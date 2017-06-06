4e0fc30a87d5d1ccad711d376baf51f5ee5fee9d
ccggeo <georgeconnorc@gmail.com>
2017-06-06 12:32:03 +0100

    Delete ad_userexport.ps1

--- ad_userexport.ps1
+++
@@ -1,1 +0,0 @@
-﻿get-aduser -SearchBase "***" -Filter {enabled -eq $true}  -properties GivenName,  Surname, Title, mail, Department, telephoneNumber, mobile| select GivenName,  Surname, Title, mail, Department, telephoneNumber, mobile | export-csv c:\scripts\freshservice_export_withemails.csv
