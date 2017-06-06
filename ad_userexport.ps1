20a389330b0301ffc98da36be7b7bbeb3525fc70
ccggeo <georgeconnorc@gmail.com>
2017-06-06 14:29:48 +0100

    Script to gather aduserdata from AD

---
+++ ad_userexport.ps1
@@ -0,0 +1,1 @@
+﻿get-aduser -SearchBase "***" -Filter {enabled -eq $true}  -properties GivenName,  Surname, Title, mail, Department, telephoneNumber, mobile| select GivenName,  Surname, Title, mail, Department, telephoneNumber, mobile | out-gridview
