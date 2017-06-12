20d975b566c6c9c89f32b14cab3bd7f9d10ac366
ccggeo <georgeconnorc@gmail.com>
2017-06-12 09:35:37 +0100

    Changed method of slecting machines with searchbase, now description is added along with hostname  Changes to be committed:

--- last_rebootime.ps1
+++ last_rebootime.ps1
@@ -1,24 +1,34 @@
-﻿$machines=@()
+﻿
+$machines = Get-ADComputer -searchbase "***" -filter * -Properties name, description 
 
-$name = Get-ADComputer -filter * | ? { $_.Name -like "**"  } 
+$computers = $machines.name
 
-
-$machines
 
 $output =@()
 
-foreach ($comp in $machines){
+foreach ($comp in $computers){
     
-    $line = '' | select comp, lastboot 
-
+    $line = '' | select comp, lastboot, user
+    $description = (invoke-command -computername $comp -scriptblock {Get-ADComputer $comp -filter * -Properties description }).description
     $state = invoke-command -computername $comp -scriptblock {Get-WmiObject win32_operatingsystem | select csname, @{LABEL=’LastBootUpTime’;EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}
+    
+
                                                              }
-        
+   
         $line.comp = $comp
         $line.lastboot = $state.lastbootuptime
-         
+        $line.user = $description
+
+
         $output += $line
                             }
 
 $output | Out-GridView 
 
+
+
+<#
+ $timestamp = $(get-date -Format ddMMyyyy)
+$output | export-csv C:\temp\lastreboot_$timestamp.csv
+
+Invoke-Expression C:\temp\lastreboot_$timestamp.csv#>
