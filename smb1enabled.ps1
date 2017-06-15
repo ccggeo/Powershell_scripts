b100e1c5932273ce5d49823f650cc922b1961b3f
ccggeo <georgeconnorc@gmail.com>
2017-06-15 16:33:51 +0100

    .

--- smb1enabled.ps1
+++ smb1enabled.ps1
@@ -1,12 +1,38 @@
 ﻿#Grabs workstations dependant on naming scheme 
-$name = Get-ADComputer -filter * | ? { $_.Name -like "*name*" } 
 $computers =@()
 
+
+$name = Get-ADComputer -filter * | ? { $_.Name -like "*DW*"  } 
+
+
+
 $name | % {
-
    $computers += $_.name
-
 }
+
+$name = Get-ADComputer -filter * | ? { $_.Name -like "*VW*"  } 
+
+
+
+$name | % {
+   $computers += $_.name
+}
+
+$name = Get-ADComputer -filter * | ? { $_.Name -like "*LW*"  } 
+
+
+
+$name | % {
+   $computers += $_.name
+}
+
+$name = Get-ADComputer -filter * | ? { $_.Name -like "*TW*"  } 
+
+$name | % {
+   $computers += $_.name
+}
+
+$computers
 
 
 $output =@()
@@ -16,18 +42,30 @@ foreach ($comp in $computers){
     
     $line = '' | select comp, state 
 
-    $state = invoke-command -computername $comp -scriptblock {(Get-WindowsOptionalFeature -online | Where-Object {$_.FeatureName -eq "smb1protocol"})
+     invoke-command -computername $comp -scriptblock {
+    
+    $smb1 = (Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters).smb1
+                              
 
-      }
+                              if ($smb1 =1){ $state = 'Enabled'}
+                              
+                              else {$state= 'Disabled'}
+                             
+
+
+                                                             }
         
-
         $line.comp = $comp
         $line.state = $state.state
-        
          
         $output += $line
- }
- 
+                            }
+
+#$output | Out-GridView 
 
  
-$output | Out-GridView 
+
+ $timestamp = $(get-date -Format ddMMyyyy)
+$output | export-csv C:\temp\smb1_$timestamp.csv
+
+#Invoke-Expression C:\temp\$timestamp.csv
