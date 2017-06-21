d67ba8e0affad1d7af6bfe751af8980949682caf
ccggeo <georgeconnorc@gmail.com>
2017-06-21 11:26:48 +0100

    script to check username against ad computer. time saving ops

---
+++ find_username.ps1
@@ -0,0 +1,5 @@
+﻿$computername = read-host -Prompt "input Computer name"
+
+$name = get-adcomputer $computername -properties description 
+
+write-host 'User is:' $name.description -BackgroundColor Red 
