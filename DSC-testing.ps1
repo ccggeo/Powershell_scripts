d43211e844638a9ded80ac34dc36b59222e699de
ccggeo <georgeconnorc@gmail.com>
2017-06-21 15:08:17 +0100

    Began testing with DSC and local repo for service and simple 7zip install

---
+++ DSC-testing.ps1
@@ -0,0 +1,44 @@
+﻿Configuration Install7Zip
+{Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
+     Node $env:ComputerName
+     {
+         Package Install7Zip
+         {
+             Ensure = 'Present'
+             Name = '7-Zip 17.00'
+             Path = 'C:\temp\7z1700.msi'
+             ProductId = '23170F69-40C1-2701-1700-000001000000'
+         }
+     }
+
+     }
+
+     Install7Zip -outputpath c:\7zip
+
+
+
+
+Configuration w32tm
+
+{Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
+     Node $env:ComputerName
+
+     {
+         Service w32tm
+                {
+                    Name = "W32Time"
+                    StartupType = "Automatic"
+                    State = "Running"
+                    Ensure = "Present"
+                }
+
+
+     }
+
+     }
+
+     w32tm -outputpath c:\Services
+
+ #Start-DscConfiguration -path C:\Services -wait -Verbose -force
+
+
