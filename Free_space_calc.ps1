56122f1e7dc22ab70cd32ad0624b721167cbd1cb
ccggeo <georgeconnorc@gmail.com>
2017-06-28 12:41:58 +0100

    Calculates free space of array of machines

---
+++ Free_space_calc.ps1
@@ -0,0 +1,27 @@
+﻿
+$computers = 
+
+$output= @()
+
+$computers | % {
+
+$line = '' | select hostname, C, D, user
+
+$C = Get-CimInstance -ComputerName $_ win32_logicaldisk | where caption -eq "C:" | foreach-object {write " $($_.caption) $('{0:N2}' -f ($_.Size/1gb)) GB total, $('{0:N2}' -f ($_.FreeSpace/1gb)) GB free "}
+$D = Get-CimInstance -ComputerName $_ win32_logicaldisk | where caption -eq "D:" | foreach-object {write " $($_.caption) $('{0:N2}' -f ($_.Size/1gb)) GB total, $('{0:N2}' -f ($_.FreeSpace/1gb)) GB free "}
+
+$line.hostname = $_
+$line.C = $C
+$line.D = $D
+$line.user = (get-adcomputer $_).description 
+
+write-host "processing" $_ 
+ 
+ 
+ $output += $line 
+
+
+
+    }
+
+$output | Out-GridView
