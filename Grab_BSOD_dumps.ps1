06448ca13105b3c1f5d91afb5ca14f873fab1a99
ccggeo <georgeconnorc@gmail.com>
2017-08-02 11:37:24 +0100

    grabs bsod dumps from specified computer

---
+++ Grab_BSOD_dumps.ps1
@@ -0,0 +1,13 @@
+﻿
+
+
+
+
+$computer_name = read-host -Prompt "Confirm computer name"  
+
+$User = get-adcomputer $computer_name -Properties * 
+$user =$user.description
+
+
+
+robocopy \\$computer_name\c$\windows\minidump C:\temp\BSOD_dumps\$user'_dump'
