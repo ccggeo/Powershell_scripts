e844953b69237ad3b68299fc6afed3643ccadf41
ccggeo <georgeconnorc@gmail.com>
2017-08-02 11:39:21 +0100

    clean up

--- Grab_BSOD_dumps.ps1
+++ Grab_BSOD_dumps.ps1
@@ -1,13 +1,6 @@
-﻿
-
-
-
-
-$computer_name = read-host -Prompt "Confirm computer name"  
+﻿$computer_name = read-host -Prompt "Confirm computer name"  
 
 $User = get-adcomputer $computer_name -Properties * 
 $user =$user.description
-
-
 
 robocopy \\$computer_name\c$\windows\minidump C:\temp\BSOD_dumps\$user'_dump'
