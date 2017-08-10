d0bed139da863532b59458b96d2abd64b8be8925
ccggeo <georgeconnorc@gmail.com>
2017-08-10 15:40:21 +0100

    Added ability to restart search indexing

--- remove_SFB-key.ps1
+++ remove_SFB-key.ps1
@@ -1,16 +1,23 @@
-﻿invoke-command -ComputerName bs-fp-0648DW -ScriptBlock{
-pushd
-sl "Registry::HKEY_CLASSES_ROOT\TypeLib\{2DF8D04C-5BFA-101B-BDE5-00AA0044DE52}"
+﻿sl "Registry::HKEY_CLASSES_ROOT\TypeLib\{2DF8D04C-5BFA-101B-BDE5-00AA0044DE52}"
 $exists = test-path 2.8
 if ($exists -eq $true)
 {
 write-host "SFB key exists, removing key..." -ForegroundColor Cyan
-    #remove-item 2.8 
+remove-item 2.8 
+
+
+  
 }
 else
-{ write-host "Key is already removed" -ForegroundColor green
+{ write-host "SFB Key is already removed" -ForegroundColor green
     
 }
-}
 
-#get-itemproperty  -path "Registry::HKEY_CLASSES_ROOT\TypeLib\{2DF8D04C-5BFA-101B-BDE5-00AA0044DE52}\2.8"  }
+#reset searching index
+Set-ItemProperty -path "HKLM:\Software\Microsoft\Windows Search" -Name SetupCompletedSuccessfully -Value 0
+Stop-Service wsearch
+Start-Service wsearch
+
+$services = get-service -Name wsearch
+
+write-host 'Wsearch is now'$services.status -ForegroundColor yellow 
