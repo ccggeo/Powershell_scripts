859895528ef43de2d448ab503a391af58385d12b
ccggeo <georgeconnorc@gmail.com>
2017-08-10 17:14:05 +0100

    remote computers

--- remove_SFB-key.ps1
+++ remove_SFB-key.ps1
@@ -1,11 +1,9 @@
-﻿sl "Registry::HKEY_CLASSES_ROOT\TypeLib\{2DF8D04C-5BFA-101B-BDE5-00AA0044DE52}"
+﻿invoke-command -computername -scriptblock{sl "Registry::HKEY_CLASSES_ROOT\TypeLib\{2DF8D04C-5BFA-101B-BDE5-00AA0044DE52}"
 $exists = test-path 2.8
 if ($exists -eq $true)
 {
 write-host "SFB key exists, removing key..." -ForegroundColor Cyan
 remove-item 2.8 
-
-
   
 }
 else
@@ -14,10 +12,10 @@ else
 }
 
 #reset searching index
-Set-ItemProperty -path "HKLM:\Software\Microsoft\Windows Search" -Name SetupCompletedSuccessfully -Value 0
+set-ItemProperty -path "HKLM:\Software\Microsoft\Windows Search" -Name SetupCompletedSuccessfully -Value 0
 Stop-Service wsearch
 Start-Service wsearch
 
 $services = get-service -Name wsearch
 
-write-host 'Wsearch is now'$services.status -ForegroundColor yellow 
+write-host 'Wsearch is now'$services.status -ForegroundColor yellow }
