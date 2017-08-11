0cdb2dced5eda52fff836a7db48ab5b57d704128
ccggeo <georgeconnorc@gmail.com>
2017-08-11 12:51:17 +0100

    avoids admin prompt when entering a session

---
+++ no_admin_prompt.ps1
@@ -0,0 +1,8 @@
+﻿$User = "***"
+$SecurePass =  Read-Host "enter password" -AsSecureString
+$credential = New-Object System.Management.Automation.PSCredential $User, $SecurePass
+
+Get-Credential $credential 
+$hostname = hostname 
+
+Enter-PSSession -ComputerName $hostname -Credential $credential
