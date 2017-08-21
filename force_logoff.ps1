672589f3053b14a659669c47c95e28b22150fadc
ccggeo <georgeconnorc@gmail.com>
2017-08-21 16:43:52 +0100

    Script to force log off all users on a machine given a short name

---
+++ force_logoff.ps1
@@ -0,0 +1,51 @@
+﻿#force log off users from meeting room PCs. quick way to get meetings going asap and force all logged on previously off. 
+
+$x = Read-Host -Prompt 'input room name'
+
+
+if ($x -eq "X" -or $x -eq 'Y')
+{
+    write-host 'XY selected'-ForegroundColor Green
+
+    #Invoke-CimMethod -ClassName Win32_Operatingsystem -ComputerName XY  -MethodName Win32Shutdown -Arguments @{ Flags = 4 }
+}
+
+else { 
+
+if ($x -eq "X" -or $x -eq 'Y')
+{
+write-host 'XY selected'-ForegroundColor Green
+#Invoke-CimMethod -ClassName Win32_Operatingsystem -ComputerName XY  -MethodName Win32Shutdown -Arguments @{ Flags = 4 }
+}
+
+else {
+
+if ($x -eq "XY" -or $x -eq 'XY')
+{
+write-host 'XY selected'-ForegroundColor Green
+#Invoke-CimMethod -ClassName Win32_Operatingsystem -ComputerNameXY  -MethodName Win32Shutdown -Arguments @{ Flags = 4 }
+}
+else{
+                
+if ($x -eq "XY" -or $x -eq 'XY')
+{
+write-host 'XY selected'-ForegroundColor Green
+#Invoke-CimMethod -ClassName Win32_Operatingsystem -ComputerName XY  -MethodName Win32Shutdown -Arguments @{ Flags = 4 }
+else {
+                
+                
+                } 
+                
+                
+                }
+
+                
+             
+                
+                }
+
+                }
+
+
+}
+
