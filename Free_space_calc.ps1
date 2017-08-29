541b30a10d4c99510b5fcb494144fd67fbfc44d7
ccggeo <georgeconnorc@gmail.com>
2017-08-29 11:28:09 +0100

    added maths to the calculate total space used. All figures in GB

--- Free_space_calc.ps1
+++ Free_space_calc.ps1
@@ -1,17 +1,39 @@
 ﻿
-$computers = 
+$computers = ''
 
 $output= @()
 
 $computers | % {
 
-$line = '' | select hostname, C, D, user
+$line = '' | select hostname, Ctotal, Cfree, Cused, Dtotal, Dfree, Dused, user
 
-$C = Get-CimInstance -ComputerName $_ win32_logicaldisk | where caption -eq "C:" | foreach-object {write " $($_.caption) $('{0:N2}' -f ($_.Size/1gb)) GB total, $('{0:N2}' -f ($_.FreeSpace/1gb)) GB free "}
-$D = Get-CimInstance -ComputerName $_ win32_logicaldisk | where caption -eq "D:" | foreach-object {write " $($_.caption) $('{0:N2}' -f ($_.Size/1gb)) GB total, $('{0:N2}' -f ($_.FreeSpace/1gb)) GB free "}
+$Ctotal = Get-CimInstance -ComputerName $_ win32_logicaldisk | where caption -eq "C:" | foreach-object {write " $('{0:N2}' -f ($_.Size/1gb))"}
+$cfree = Get-CimInstance -ComputerName $_ win32_logicaldisk | where caption -eq "C:" | foreach-object {write "$('{0:N2}' -f ($_.FreeSpace/1gb)) "}
+
+#calculate used space on c drive
+$cused = $ctotal - $cfree
+
+
+$D = Get-CimInstance -ComputerName $_ win32_logicaldisk | where caption -eq "D:" | foreach-object {write " $($_.caption) $('{0:N2}' -f ($_.Size/1gb)) , $('{0:N2}' -f ($_.FreeSpace/1gb))  "}
+
+$dtotal = Get-CimInstance -ComputerName $_ win32_logicaldisk | where caption -eq "D:" | foreach-object {write " $('{0:N2}' -f ($_.Size/1gb))"}
+$dfree = Get-CimInstance -ComputerName $_ win32_logicaldisk | where caption -eq "D:" | foreach-object {write "$('{0:N2}' -f ($_.FreeSpace/1gb)) "}
+#calculate used space on d drive
+$dused = $dtotal - $dfree
+
 
 $line.hostname = $_
-$line.C = $C
+$line.Ctotal = $Ctotal
+$line.Cfree = $Cfree
+$line.Cused = $cused
+
+$line.dtotal = $dtotal
+$line.dfree = $dfree
+$line.dused = $dused
+
+
+
+
 $line.D = $D
 $line.user = (get-adcomputer $_).description 
 
