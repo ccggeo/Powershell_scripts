0f8fd68cac4c809e202813cd14256147383596da
ccggeo <georgeconnorc@gmail.com>
2017-08-30 11:29:37 +0100

    Finds AD group membership and copies it to another user

---
+++ copy_group_membership.ps1
@@ -0,0 +1,18 @@
+﻿Import-Module ActiveDirectory
+
+
+$user_copy = read-host -Prompt "input old user to copy membership from"
+$new_user = read-host -Prompt "input new User to copy membership to"
+
+$group_membership = Get-ADPrincipalGroupMembership $user_copy | select name 
+
+
+
+$group_membership | % { 
+
+add-adgroupmember -identity $_.name -members $new_user
+
+
+}
+
+
