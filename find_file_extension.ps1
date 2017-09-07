40529122c1a5c7a0a4cb81d59ad0b4240258b8f3
ccggeo <georgeconnorc@gmail.com>
2017-09-07 09:29:48 +0100

    Find files within a directory based on extension

---
+++ find_file_extension.ps1
@@ -0,0 +1,5 @@
+﻿$Dir = get-childitem "" -recurse
+# $Dir |get-member
+$List = $Dir | where {$_.extension -eq ".indd"}
+$List |  ft fullname |out-file C:\Scripts\indd.txt
+# List | format-table name
