1a5074533e666b13ad9d8c29249e37778a4e7ed1
ccggeo <georgeconnorc@gmail.com>
2017-09-07 15:29:40 +0100

    for entry into ad

---
+++ format_phone.ps1
@@ -0,0 +1,19 @@
+﻿ function format_phone{
+    $aduser = get-aduser <user> -Properties * 
+    $sfbphone = $aduser.officephone 
+    $sfbphone
+
+    $sfbphone = $sfbphone.replace(' ','')
+
+    $sfbphone= $sfbphone.Insert(3," ")
+    $sfbphone= $sfbphone.Insert(4,"(")
+    $sfbphone= $sfbphone.Insert(5,"0")
+    $sfbphone= $sfbphone.Insert(6,")")
+    $sfbphone= $sfbphone.Insert(11," ")
+    $sfbphone= $sfbphone.Insert(15," ")
+
+    set-aduser -Identity $aduser -officephone $SFBphone
+    $sfbphone
+    }
+
+format_phone
