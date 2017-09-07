43714ed9f4ea4279ffddedc02cdca9d77cc3d177
ccggeo <georgeconnorc@gmail.com>
2017-09-07 16:42:55 +0100

    added exception handling and ability to see which user logged on

--- force_logoff.ps1
+++ force_logoff.ps1
@@ -1,51 +1,125 @@
-﻿#force log off users from meeting room PCs. quick way to get meetings going asap and force all logged on previously off. 
+﻿#force log off all users from  room PCs 
 
-$x = Read-Host -Prompt 'input room name'
+            $x = Read-Host -Prompt 'Input first two letters of room name' 
 
 
-if ($x -eq "X" -or $x -eq 'Y')
+            if ($x -eq "" -or $x -eq '')
+            {
+             try {
+           
+           
+            write-host ' selected: checking for logged on users...'-ForegroundColor cyan
+            $user_gr= Get-CimInstance –ComputerName  –ClassName Win32_ComputerSystem 
+                   
+            Invoke-CimMethod -ClassName Win32_Operatingsystem -ComputerName   -MethodName Win32Shutdown -Arguments @{ Flags = 4 } -ErrorAction Stop | out-null
+            write-host $user_gr.username 'currently logged on...' -ForegroundColor yellow
+
+            write-host 'All users now logged off' -ForegroundColor green 
+
+
+            }
+
+            catch{write-host 'no one is logged onto ' -BackgroundColor red}
+            
+            }
+
+            else { 
+
+            if ($x -eq "" -or $x -eq '')
 {
-    write-host 'XY selected'-ForegroundColor Green
+                   try {
+                     
+            write-host ' selected: checking for logged on users...'-ForegroundColor cyan
+            $user_po= Get-CimInstance –ComputerName  –ClassName Win32_ComputerSystem 
+            
+            Invoke-CimMethod -ClassName Win32_Operatingsystem -ComputerName  -MethodName Win32Shutdown -Arguments @{ Flags = 4 } -ErrorAction Stop | out-null
+            write-host $user_po.username 'currently logged on...' -ForegroundColor yellow
+            write-host 'All users now logged off' -ForegroundColor green 
 
-    #Invoke-CimMethod -ClassName Win32_Operatingsystem -ComputerName XY  -MethodName Win32Shutdown -Arguments @{ Flags = 4 }
-}
 
-else { 
+            }
 
-if ($x -eq "X" -or $x -eq 'Y')
-{
-write-host 'XY selected'-ForegroundColor Green
-#Invoke-CimMethod -ClassName Win32_Operatingsystem -ComputerName XY  -MethodName Win32Shutdown -Arguments @{ Flags = 4 }
-}
+            catch{write-host 'no one is logged onto ' -BackgroundColor red}                                   
+                                     
+                                     }
 
-else {
 
-if ($x -eq "XY" -or $x -eq 'XY')
-{
-write-host 'XY selected'-ForegroundColor Green
-#Invoke-CimMethod -ClassName Win32_Operatingsystem -ComputerNameXY  -MethodName Win32Shutdown -Arguments @{ Flags = 4 }
-}
-else{
+                        else {
+
+                if ($x -eq "" -or $x -eq '')
+                {
+                                 try {
+           
+           
+            write-host ' selected: checking for logged on users...'-ForegroundColor cyan
+            
+            write-host $user_sa.username 'currently logged on...' -ForegroundColor yellow
+
+            Invoke-CimMethod -ClassName Win32_Operatingsystem -ComputerName   -MethodName Win32Shutdown -Arguments @{ Flags = 4 } -ErrorAction Stop | out-null
+            $user_sa= Get-CimInstance –ComputerName  –ClassName Win32_ComputerSystem 
+            write-host 'All users now logged off' -ForegroundColor green 
+
+
+            }
+
+
+            catch{write-host 'no one is logged onto ' -BackgroundColor red}
                 
-if ($x -eq "XY" -or $x -eq 'XY')
-{
-write-host 'XY selected'-ForegroundColor Green
-#Invoke-CimMethod -ClassName Win32_Operatingsystem -ComputerName XY  -MethodName Win32Shutdown -Arguments @{ Flags = 4 }
-else {
+         
+
                 
+                }
+                else{
+          
                 
+                             
+                if ($x -eq "" -or $x -eq '')
+                {
+                     try {
+           
+           
+            write-host ' selected: checking for logged on users...'-ForegroundColor cyan
+            
+            write-host $user_tu.username 'currently logged on...' -ForegroundColor yellow
+
+            Invoke-CimMethod -ClassName Win32_Operatingsystem -ComputerName   -MethodName Win32Shutdown -Arguments @{ Flags = 4 } -ErrorAction Stop | out-null
+            $user_tu= Get-CimInstance –ComputerName  –ClassName Win32_ComputerSystem 
+            write-host 'All users now logged off' -ForegroundColor green 
+
+
+            }
+
+
+            catch{write-host 'no one is logged onto ' -BackgroundColor red}                           
+                   
                 } 
+                                                    
+                else{
                 
+                if ($x -eq "" -or $x -eq 're')
+                {
+                                try {
+                
+            write-host ' selected: checking for logged on users...'-ForegroundColor cyan
+            
+            write-host $user_re.username 'currently logged on...' -ForegroundColor yellow
+        
+            Invoke-CimMethod -ClassName Win32_Operatingsystem -ComputerName   -MethodName Win32Shutdown -Arguments @{ Flags = 4 } -ErrorAction Stop | out-null
+            $user_re= Get-CimInstance –ComputerName  –ClassName Win32_ComputerSystem 
+            write-host 'All users now logged off' -ForegroundColor green 
+
+            }
+
+            catch{write-host 'no one is logged onto Reception' -BackgroundColor red}
+   
+                
+                }
+                else {write-host $x 'is not a valid BS computer name ' -BackgroundColor RED }                 
+           
+                }
                 
                 }
 
-                
-             
-                
                 }
-
-                }
-
 
 }
-
