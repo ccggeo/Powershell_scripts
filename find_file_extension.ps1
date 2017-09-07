ed192619d647acfe111668baf5e868cbf12d42af
ccggeo <georgeconnorc@gmail.com>
2017-09-07 09:38:15 +0100

    also allowed extension to become output file name

--- find_file_extension.ps1
+++ find_file_extension.ps1
@@ -3,7 +3,5 @@ $extension_ = read-host "Enter extension"
 
 
 $Dir = get-childitem "$path_" -recurse
-# $Dir |get-member
 $List = $Dir | where {$_.extension -eq ".$extension_"}
 $List |  ft fullname |out-file C:\Scripts\$extension_.txt
-# List | format-table name
