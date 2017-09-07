56440915c7182b17b0401af19d81e4cfadede473
ccggeo <georgeconnorc@gmail.com>
2017-09-07 09:34:40 +0100

    input data from user

--- find_file_extension.ps1
+++ find_file_extension.ps1
@@ -1,5 +1,9 @@
-﻿$Dir = get-childitem "" -recurse
+﻿$path_ = read-host "Enter path to search" 
+$extension_ = read-host "Enter extension" 
+
+
+$Dir = get-childitem "$path_" -recurse
 # $Dir |get-member
-$List = $Dir | where {$_.extension -eq ".indd"}
-$List |  ft fullname |out-file C:\Scripts\indd.txt
+$List = $Dir | where {$_.extension -eq ".$extension_"}
+$List |  ft fullname |out-file C:\Scripts\$extension_.txt
 # List | format-table name
