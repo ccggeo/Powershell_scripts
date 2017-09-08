76f3e00ec55509c585b6fcecd9ecd761554135b6
ccggeo <georgeconnorc@gmail.com>
2017-09-08 13:59:26 +0100

    add ability to reset windows indexing

--- remove_SFB-key.ps1
+++ remove_SFB-key.ps1
@@ -1,4 +1,8 @@
-﻿invoke-command -computername -scriptblock{sl "Registry::HKEY_CLASSES_ROOT\TypeLib\{2DF8D04C-5BFA-101B-BDE5-00AA0044DE52}"
+﻿invoke-command -computername  -scriptblock{
+
+winrm qc -force
+
+sl "Registry::HKEY_CLASSES_ROOT\TypeLib\{2DF8D04C-5BFA-101B-BDE5-00AA0044DE52}"
 $exists = test-path 2.8
 if ($exists -eq $true)
 {
@@ -9,13 +13,13 @@ remove-item 2.8
 else
 { write-host "SFB Key is already removed" -ForegroundColor green
     
-}
+}}
 
-#reset searching index
+<##reset searching index
 set-ItemProperty -path "HKLM:\Software\Microsoft\Windows Search" -Name SetupCompletedSuccessfully -Value 0
 Stop-Service wsearch
 Start-Service wsearch
 
 $services = get-service -Name wsearch
 
-write-host 'Wsearch is now'$services.status -ForegroundColor yellow }
+write-host 'Wsearch is now'$services.status -ForegroundColor yellow }#>
