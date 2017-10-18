0c6f1170f65bf6937764f94d3b032dcd2c42dd74
ccggeo <georgeconnorc@gmail.com>
2017-10-18 16:40:13 +0100

    commits

---
+++ Exchangesync_mobiles.ps1
@@ -0,0 +1,33 @@
+﻿#get-mobiledevice  | select identity, devicetype, deviceuseragent, DeviceOS, Devicemodel, Firstsynctime, Ismanaged, iscompliant, isdisabled | Out-GridView
+
+
+$users = get-aduser -SearchBase "(((" -Filter {enabled -eq $true}  -properties *
+
+$x = $users.mailnickname 
+$output= @()
+
+$x | % {
+
+$line = '' | select username, connected_devices_Total, devicetype, deviceuseragent, DeviceOS, Devicemodel, Firstsynctime
+$usermobiles = get-mobiledevice -Mailbox $_ |select devicetype, deviceuseragent, DeviceOS, Devicemodel, Firstsynctime, Ismanaged, iscompliant, isdisabled
+
+
+$line.username = $_
+$line.connected_devices_Total = $usermobiles.count
+$line.devicetype = $usermobiles.devicetype -join ', '
+$line.deviceuseragent = $usermobiles.deviceuseragent -join ', '
+$line.DeviceOS = $usermobiles.deviceOS -join ', '
+$line.Devicemodel = $usermobiles.devicemodel -join ', '
+$line.Firstsynctime = $usermobiles.firstsynctime -join ', '
+
+
+$line
+$output += $line 
+
+ 
+
+}
+
+
+
+$output | export-csv c:\scripts\mobileExport.csv
---
+++ GetSMBfile.ps1
@@ -0,0 +1,13 @@
+﻿$openFiles = get-smbopenfile -CimSession <server>  | Where-Object {$_.path -like "**"}
+
+
+$openFiles | Sort-Object -Property path
+
+$fileID = $openFiles.fileid
+$fileID
+
+$fileID | %{
+Close-SmbOpenFile -CimSession <server> $_ }
+$fileID
+
+
