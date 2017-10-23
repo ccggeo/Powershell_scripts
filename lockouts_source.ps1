773908f49a62598c9c3ce288bd0e1f0b8b5ad037
ccggeo <georgeconnorc@gmail.com>
2017-10-23 10:34:09 +0100

    find source of lockouts for user

---
+++ lockouts_source.ps1
@@ -0,0 +1,13 @@
+﻿#Obtain user
+$User = ""
+
+#Specify DC
+$DC = Get-ADDomainController  <DC>
+
+#Collect lockout events for user based on time 
+
+$timespan = new-timespan -hours 5 |Select-Object -ExpandProperty totalmilliseconds
+
+Get-WinEvent -ComputerName $DC -Logname Security -FilterXPath "*[System[EventID=4740 and TimeCreated[timediff(@SystemTime) <= $timespan]] and EventData[Data[@Name='TargetUserName']='$User']]" |
+
+Select-Object TimeCreated,@{Name='User Name';Expression={$_.Properties[0].Value}},@{Name='Source Host';Expression={$_.Properties[1].Value}}
