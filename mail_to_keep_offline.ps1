19e1d9827ee9186ab7345c26f4a6024d626f34ee
ccggeo <georgeconnorc@gmail.com>
2017-11-13 14:18:26 +0000

    Change cached offline mail in outlook

---
+++ mail_to_keep_offline.ps1
@@ -0,0 +1,19 @@
+﻿$user = ""
+
+invoke-command -computername <> -ScriptBlock{
+
+
+
+$objUser = New-Object System.Security.Principal.NTAccount("$user")
+$strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+
+$SID = $strSID.Value
+
+Set-ItemProperty "Microsoft.PowerShell.Core\Registry::HKEY_USERS\$SID\SOFTWARE\Policies\Microsoft\office\15.0\outlook\cached mode" -Name "cachedexchangemode" -Value 0
+
+
+$days = (Get-ItemProperty "Microsoft.PowerShell.Core\Registry::HKEY_USERS\$SID\SOFTWARE\Policies\Microsoft\office\15.0\outlook\cached mode" -Name "cachedexchangemode").cachedexchangemode
+
+write-host "Mail to keep offline = $days days" -ForegroundColor green 
+
+}
