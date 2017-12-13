d8fd48ebb6bb9511e848bbb496eb5e9189131634
ccggeo <georgeconnorc@gmail.com>
2017-12-13 17:01:20 +0000

    Unction to create signature

--- create-signature.ps1
+++ create-signature.ps1
@@ -1,18 +1,22 @@
 ﻿#create signature 
-$new_user_name = "Maya Jane"
+$new_user_name = Read-host 'Enter username of new signature'  
 
 #creates the signature locally 
 function create-signature {
 
 param($adusername)
 
-$name = $new_user_name
+$user = get-aduser -Properties * -filter * | Where-Object {$_.mailnickname  -like "*$adusername*"}
+
+$name = $user.name 
+$title = $user.title 
+$sfbphone = $user.telephonenumber
 $username = $user.mailnickname 
 $local_username = ""
 $Signature = Get-Content C:\Users\$local_username\Desktop\Signature_creator\signature_template_.txt
 
 
-write-host "Creating $name signature"
+write-host "Creating $name signature" -BackgroundColor blue
 
 #template in HTML with placeholders
 (Get-Content C:\Users\$local_username\Desktop\Signature_creator\Signature_template_.txt).replace('$name', $name) | Set-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v1.txt
@@ -21,9 +25,9 @@ write-host "Creating $name signature"
 (Get-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v3.txt).replace('$username', $username) | Set-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\final.htm
 
 Get-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v3.txt | Set-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\final_text.txt
-Get-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v3.txt | Set-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\final_rtf.rtf}
-
-
+Get-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v3.txt | Set-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\final_rtf.rtf
+invoke-expression C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\final.htm
+}
 
 create-signature -adusername $new_user_name
 
