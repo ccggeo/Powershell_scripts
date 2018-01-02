65765e4a0ab67088b45e34a6a83b80e2c935e20d
ccggeo <georgeconnorc@gmail.com>
2018-01-02 15:09:16 +0000

    Correct function parameter

--- New_starter.ps1
+++ New_starter.ps1
@@ -76,31 +76,35 @@ Send-MailMessage
 #creates the signature locally 
 function create-signature {
 
-param($adusername)
+ Param
+        (
+            [Parameter(Mandatory=$true)]
+            $adusername)
 
-$user = get-aduser -Properties * -filter * | Where-Object {$_.mailnickname  -like "*$adusername*"}
+    $user = get-aduser -Properties * -filter * | Where-Object {$_.mailnickname  -like "*$adusername*"}
+    $name = $user.name 
+    $title = $user.title 
+    $sfbphone = $user.telephonenumber
+    $username = $user.mailnickname 
+    $local_username = ""
+    $Signature = Get-Content C:\Users\$local_username\Desktop\Signature_creator\signature_template_.txt
 
-$name = $user.name 
-$title = $user.title 
-$sfbphone = $user.telephonenumber
-$username = $user.mailnickname 
-$local_username = ""
-$Signature = Get-Content C:\Users\$local_username\Desktop\Signature_creator\signature_template_.txt
+    write-host "Creating $name signature" -BackgroundColor blue
 
-write-host "Creating $name signature" -BackgroundColor blue
+    #template in HTML with placeholders
+    (Get-Content C:\Users\$local_username\Desktop\Signature_creator\Signature_template_.txt).replace('$name', $name) | Set-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v1.txt
+    (Get-Content  C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v1.txt).replace('$title', $title) | Set-Content  C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v2.txt
+    (Get-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v2.txt).replace('$phone', $sfbphone) | Set-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v3.txt
+    (Get-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v3.txt).replace('$username', $username) | Set-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\final.htm
 
-#template in HTML with placeholders
-(Get-Content C:\Users\$local_username\Desktop\Signature_creator\Signature_template_.txt).replace('$name', $name) | Set-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v1.txt
-(Get-Content  C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v1.txt).replace('$title', $title) | Set-Content  C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v2.txt
-(Get-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v2.txt).replace('$phone', $sfbphone) | Set-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v3.txt
-(Get-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v3.txt).replace('$username', $username) | Set-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\final.htm
+    Get-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v3.txt | Set-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\final_text.txt
+    Get-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v3.txt | Set-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\final_rtf.rtf
+    invoke-expression C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\final.htm
+    
+    }
 
-Get-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v3.txt | Set-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\final_text.txt
-Get-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\v3.txt | Set-Content C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\final_rtf.rtf
-invoke-expression C:\Users\$local_username\Desktop\Signature_creator\PS_Tests\final.htm
-}
+create-signature -adusername $new_user_name
 
-create-signature -adusername $new_user_username  
 
 
 
