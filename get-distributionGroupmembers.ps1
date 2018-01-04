0cfb1e856b0af90ab8f257968f39a18b1df8543f
ccggeo <georgeconnorc@gmail.com>
2018-01-04 12:59:24 +0000

    get DG members based on OU

---
+++ get-distributionGroupmembers.ps1
@@ -0,0 +1,52 @@
+﻿$x = (get-distributiongroup -OrganizationalUnit "").primarysmtpaddress
+$output= @()
+
+
+$x | %{
+$line = '' | select distributiongroup, members 
+
+$y = (Get-DistributionGroupMember $_).name
+
+
+#$c = [string]$y
+
+$line.distributiongroup = $_
+$line.members = $y -join ', '
+
+$line
+$output += $line 
+
+
+}
+$output | export-csv c:\temp\DG_members.csv
+
+
+
+
+
+
+
+$DG = (import-csv c:\temp\DG_members.csv).distributiongroup
+
+$DG | % {
+
+if ($_ -eqif ($x -gt $y)
+{
+    
+} $y)
+{
+    
+}
+
+
+
+}
+
+
+
+$DG.distributiongroup 
+
+
+
+
+
