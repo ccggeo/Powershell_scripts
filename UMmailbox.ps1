d52e41f47085e64e718b830234af657c2cce7a18
ccggeo <georgeconnorc@gmail.com>
2018-01-08 17:53:03 +0000

    find Ummailbox status

---
+++ UMmailbox.ps1
@@ -0,0 +1,13 @@
+﻿#if everything in disabled OU set UMMailbox 
+
+
+$usernames = (get-aduser -searchbase "" -Properties * -filter *).mailnickname 
+
+
+$usernames | %{
+
+$uMenabled = Get-UMMailbox $_
+
+$umenabled.umenabled 
+
+} 
