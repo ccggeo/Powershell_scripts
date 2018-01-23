a635e73f16b47cf4e93df8e983e918d1731f6ccb
ccggeo <georgeconnorc@gmail.com>
2018-01-23 10:37:12 +0000

    track messages within exchange org

---
+++ MessageTracking_.ps1
@@ -0,0 +1,22 @@
+﻿$server  = ""
+
+
+#By Sender
+$sender = ""
+get-messagetrackinglog  -Server $server -Sender $sender -Start (Get-Date).AddDays(-7) -End (Get-Date) -ResultSize 9999999 -Verbose | select Timestamp, MessageSubject, Sender, Recipients, Totalbytes | Sort-Object -Property Timestamp 
+ 
+#By Recipient
+ 
+$recipient = ""
+ 
+get-messagetrackinglog -recipient $recipient -Server $server   -Start (Get-Date).Adddays(-1) -End (Get-Date) -ResultSize 9999999 -Verbose | select Timestamp, MessageSubject, Sender, Recipients, Totalbytes | sort-object -property Timestamp 
+ 
+ 
+# By recipient and sender
+ 
+$recipient = ""
+$sender = ""
+ 
+ 
+get-messagetrackinglog -recipient $recipient -Sender $sender -Server $server  -Start (Get-Date).AddDays(-2) -End (Get-Date) -ResultSize 9999999 -Verbose
+
