ae9cd4c3f916dbf649275fe78fdde629c2759c88
ccggeo <georgeconnorc@gmail.com>
2018-01-24 14:35:29 +0000

    Ability to see leftover ram of target

---
+++ freeRAM.ps1
@@ -0,0 +1,15 @@
+﻿invoke-command -ComputerName -ScriptBlock {
+
+
+$os = Get-Ciminstance Win32_OperatingSystem
+
+$pctFree = [math]::Round(($os.FreePhysicalMemory/$os.TotalVisibleMemorySize)*100,2)
+
+$os | Select @{Name = "PctFree"; Expression = {$pctFree}},
+@{Name = "FreeGB";Expression = {[math]::Round($_.FreePhysicalMemory/1mb,2)}},
+@{Name = "TotalGB";Expression = {[int]($_.TotalVisibleMemorySize/1mb)}}
+
+}
+
+
+
