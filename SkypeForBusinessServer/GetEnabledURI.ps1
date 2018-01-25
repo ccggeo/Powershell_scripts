8cd2dab7126c3b3c9847ead4079f4b9f931e013b
ccggeo <georgeconnorc@gmail.com>
2018-01-25 16:01:11 +0000

    Get LineURI and Enabled user

---
+++ SkypeForBusinessServer/GetEnabledURI.ps1
@@ -0,0 +1,42 @@
+﻿
+
+$username = (get-csuser).samaccountname
+ $output= @()
+
+$username | % {
+
+
+
+
+        $x = (get-csuser $_)
+        $enabled = $x.ExUmEnabled
+
+     
+
+        if ($enabled -eq "true")
+            {
+            write-host "$_ enabled" -foregroundcolor green 
+           $line = '' | select user, lineuri 
+
+         $line.user = $_ | out-string
+         $line.lineuri = $x.lineuri | out-string
+
+            $output += $line
+
+    
+            }
+
+
+
+
+        else
+            {
+                write-host "user $_ disabled for SFB" -ForegroundColor red 
+
+            }
+
+          $output | Out-GridView
+
+}
+
+
