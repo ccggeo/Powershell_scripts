66b898fab840f23c81d803f7354f2b033e9694b8
ccggeo <georgeconnorc@gmail.com>
2018-01-29 12:00:57 +0000

    force install brackets and gimp

---
+++ install_brackets_gimp.ps1
@@ -0,0 +1,17 @@
+﻿
+
+invoke-command -ComputerName <computername> -ScriptBlock{
+
+    set-executionpolicy bypass
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
+    
+    choco install  brackets, gimp -y
+    
+    Remove-Item -Recurse -Force "$env:ChocolateyInstall" -WhatIf
+    [System.Text.RegularExpressions.Regex]::Replace([Microsoft.Win32.Registry]::CurrentUser.OpenSubKey('Environment').GetValue('PATH', '', [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames).ToString(), [System.Text.RegularExpressions.Regex]::Escape("$env:ChocolateyInstall\bin") + '(?>;)?', '', [System.Text.RegularExpressions.RegexOptions]::IgnoreCase) | %{[System.Environment]::SetEnvironmentVariable('PATH', $_, 'User')}
+    [System.Text.RegularExpressions.Regex]::Replace([Microsoft.Win32.Registry]::LocalMachine.OpenSubKey('SYSTEM\CurrentControlSet\Control\Session Manager\Environment\').GetValue('PATH', '', [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames).ToString(),  [System.Text.RegularExpressions.Regex]::Escape("$env:ChocolateyInstall\bin") + '(?>;)?', '', [System.Text.RegularExpressions.RegexOptions]::IgnoreCase) | %{[System.Environment]::SetEnvironmentVariable('PATH', $_, 'Machine')}      
+    set-executionpolicy restrict
+    
+    }
+
+
