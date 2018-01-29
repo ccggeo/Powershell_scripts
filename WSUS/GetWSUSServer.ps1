3f1a50483eb48b7806033f9e7eba2bf58146d49e
ccggeo <georgeconnorc@gmail.com>
2018-01-29 12:03:25 +0000

    WSUS scripts

---
+++ WSUS/GetWSUSServer.ps1
@@ -0,0 +1,38 @@
+﻿$y = get-adcomputer -searchbase "" -filter * -properties * | select name 
+
+$output= @()
+
+$y | % {
+
+$BOOL = Test-Connection -Computername $y -BufferSize 16 -Count 1 -Quiet
+
+if ($BOOL -eq $TRUE)
+{ write-host "processing $Y" -foregroundcolor green
+    
+$line = '' | select PSComputername, wsusstatus 
+
+$wsusstatus = (get-itemproperty Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate).WUSERVER 
+
+$line.PSComputername = $_ | out-string
+$line.wsusstatus = $wsusstatus | out-string
+
+$line
+$output += $line 
+
+$output
+
+
+}
+
+else {
+
+    write-host "$_ is offline" -ForegroundColor red 
+
+
+}
+
+
+}
+
+
+
