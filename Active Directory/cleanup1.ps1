261abd2810aed583da7d4a771c1bcc3adbfec8fe
ccggeo <georgeconnorc@gmail.com>
2018-01-30 16:08:54 +0000

    cleanup description fields

---
+++ Active Directory/cleanup1.ps1
@@ -0,0 +1,9 @@
+﻿$data = import-csv C:\temp\For_AD.csv
+
+$data | % {
+$mac =  $_.Mac
+$user = $_.User
+
+set-adcomputer $mac -Description $user 
+get-adcomputer $mac -Properties * | select Name, description 
+}
