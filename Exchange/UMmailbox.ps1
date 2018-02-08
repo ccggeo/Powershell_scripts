3715a2dda1566517316a2618da142eeaa89d6a97
ccggeo <georgeconnorc@gmail.com>
2018-02-08 17:23:02 +0000

    Exchange additions

---
+++ Exchange/UMmailbox.ps1
@@ -0,0 +1,25 @@
+﻿#if everything in disabled OU set UMMailbox 
+
+$usernames = (get-aduser -searchbase "" -Properties * -filter *).mailnickname 
+
+
+$usernames | %{
+
+$uMenabled = Get-UMMailbox $_
+
+
+
+if ($umenabled.umenabled  -eq "True")
+{
+ Disable-UMMailbox $_ 
+ write-host "$_ UMMailbox disabled"  -ForegroundColor green
+}
+else{
+write-host "$_ UMMailbox not enabled" -ForegroundColor red
+}
+
+
+}}
+
+
+
