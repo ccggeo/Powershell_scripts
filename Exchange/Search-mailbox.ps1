7ae910396a2a3ff95ff9b496d629f085f94fe113
ccggeo <georgeconnorc@gmail.com>
2018-02-08 17:03:20 +0000

    query mailbox for search items

---
+++ Exchange/Search-mailbox.ps1
@@ -0,0 +1,5 @@
+﻿$searchmail = #Inbox to search for 
+$ownmailbox = #your own mailbox 
+$target = #Target mail
+
+Search-Mailbox -SearchQuery "from:$searchmail" -TargetMailbox $ownmailbox -TargetFolder "Drafts" -LogLevel Full -Verbose -Identity $target
