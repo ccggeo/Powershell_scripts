e979eeed20c9aa382e5b84752453a35d5f8eb62e
ccggeo <georgeconnorc@gmail.com>
2018-02-13 11:55:14 +0000

    PS scripts for Librenms operations

---
+++ Librenms/addDevice.ps1
@@ -0,0 +1,15 @@
+﻿$HTTPHeaders = @{}
+
+$HTTPHeaders.Add('X-Auth-Token', '')
+
+$URL = ''
+
+$body = @{}
+$body.add("hostname", "")
+$body.add("Community", "")
+$body.add("force_add", "$TRUE")
+$body.add("version", "v1")
+
+$JSON = $body | ConvertTo-Json 
+
+Invoke-RestMethod -Method POST -Uri $URL -Headers $HTTPHeaders -Body $JSON -Verbose
---
+++ Librenms/getdevices.ps1
@@ -0,0 +1,11 @@
+﻿$HTTPHeaders = @{}
+
+$HTTPHeaders.Add('X-Auth-Token', '')
+
+# GET example
+$URL = ''
+$devices = Invoke-RestMethod -Method Get -Uri $URL -Headers $HTTPHeaders 
+
+$d = $devices.devices 
+
+$d.sysname 
