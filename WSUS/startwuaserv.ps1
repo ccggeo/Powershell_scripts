aa15eb39f8845a0298349c743b87aa26f26be00b
ccggeo <georgeconnorc@gmail.com>
2018-02-21 17:20:19 +0000

    update services job

---
+++ WSUS/startwuaserv.ps1
@@ -0,0 +1,19 @@
+﻿$computers= Get-ADComputer -SearchBase "OU=Windows,OU=Black Sun Workstations,DC=blacksun,DC=com" -Filter {enabled -eq $true} 
+ 
+$output= @()
+$computers| % {
+    
+    $line= '' | Select DNSHostname,UpdateStatus
+     Invoke-Command -ComputerName $_.DNSHostname -ScriptBlock { Get-Service wuauserv | ? {$_.status –eq 'Stopped'} | Start-Service}
+
+
+$line.DNSHostname = $_.DNSHostname
+$line.updatestatus = Invoke-Command -ComputerName $_.DNSHostname -ScriptBlock { (get-service wuauserv).status}
+  $line
+  $output +=$line
+             }
+                   
+
+ 
+$timestamp = $(get-date -Format HHmmddMMyyyy)
+$output | export-csv C:\Users\cgeorge\Documents\WSUS\post-service-$timestamp.csv
