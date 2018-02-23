e09879504c84a0a050fc84b54d39ced68941bf88
ccggeo <georgeconnorc@gmail.com>
2018-02-23 17:48:08 +0000

    Grab syslog dumps

---
+++ Logging/get-logs.ps1
@@ -0,0 +1,15 @@
+﻿# get last written file in log directory
+$logfilefolder = "***"
+$logfile = (Get-ChildItem $logfilefolder | Sort-Object LastWriteTime -Descending)[0].FullName
+ 
+write-host "Using $logfile"
+get-content $logfile  -Wait | % {
+ 
+ 
+ 
+    if ($_ -like "**" )
+    {
+        $_
+    }
+   
+    }
