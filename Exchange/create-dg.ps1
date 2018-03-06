ffc82154fb6b8aed809acef76c44dcc2b7a8380b
ccggeo <georgeconnorc@gmail.com>
2018-03-06 16:38:48 +0000

    add distrubtion group

---
+++ Exchange/create-dg.ps1
@@ -0,0 +1,21 @@
+﻿$dg = 
+
+$emails = @()
+
+$members = (Get-DistributionGroupMember  debeers).Name
+$members | % {
+
+    $member = $_ 
+
+    $email = (get-aduser  -Properties * -filter * | ? {$_.name  -like "*$member*"}).emailaddress
+
+    $emails += $email 
+    $emails.count
+}
+
+
+
+$dg | %{
+New-DistributionGroup $_ -members 
+ 
+}
