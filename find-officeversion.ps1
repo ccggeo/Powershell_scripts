b239054ce458383628a5c90d13f4d2c279e2bcdf
ccggeo <georgeconnorc@gmail.com>
2018-03-06 17:24:36 +0000

    new ps-object

--- find-officeversion.ps1
+++ find-officeversion.ps1
@@ -2,27 +2,19 @@
 $c = 
 
 $output= @()
-$c | %{
-
-    $line= '' | Select hostname,excelversion
+$c | %{ 
     #Creating a new PS-Object 
     $y = New-Object  Psobject  
-    $x = 
-       $y | Add-Member NoteProperty -Name "ExcelProductVersion" -Value (invoke-command -computername $_ -ScriptBlock {
-    
+    $y | Add-Member NoteProperty -Name "ExcelProductVersion" -Value (invoke-command -computername $_ -ScriptBlock {
     $excelPath =  'C:\Program Files (x86)\Microsoft Office\Office15\EXCEL.EXE' 
-    $excelProperty = (Get-ItemProperty $excelPath -Filter *)
+    $excelProperty = (Get-ItemProperty $excelPath -Filter *) 
     $excelproperty.VersionInfo.ProductVersion 
-       #excel 
-
    })
- 
- 
-    $line.hostname = $_
-    $line.excelversion = $y
-    $output += $line
+   $y | add-member noteproperty -name "hostname" -value $_ 
 
-}
+    $output += $y
+   }
+
  $output | FT
-#$excelProperty | select-object -Property 
+
 
